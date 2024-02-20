359bc26b147a02692af5f2c554e9f4fda5bbd225
hyoj6 <hyo6004@naver.com>
2024-02-20 20:44:08 +0900

    9/1/2024 search and home

--- app/src/main/java/online/bottles/api/response/bottles.kt
+++ app/src/main/java/online/bottles/api/response/bottles.kt
@@ -3,10 +3,10 @@ package online.bottles.api.response
 class bottles {
 }
 object bottlesUrl{
-    const val bottlesURL = "http://14.4.145.80:8080"
-    const val bottlesValid="http://14.4.145.80:8080/api/auth/validate-token/"
-    const val bottlesSearch = "http://14.4.145.80:8080/api/search/user/"
-    const val bottlesLogin = "http://14.4.145.80:8080/api/auth/login/"
-    const val bottlesRegister ="http://14.4.145.80:8000/api/users/"
-    const val bottlesLogout = "http://14.4.145.80:8000/api/auth/logout/"
+    const val bottlesURL = "https://www.bottlesbe.shop/"
+    const val bottlesValid="https://www.bottlesbe.shop/api/auth/validate-token/"
+    const val bottlesSearch = "https://www.bottlesbe.shop/api/search/user/"
+    const val bottlesLogin = "https://www.bottlesbe.shop/api/auth/login/"
+    const val bottlesRegister ="https://www.bottlesbe.shop/api/users/"
+    const val bottlesLogout = "https://www.bottlesbe.shop/auth/logout/"
 }
